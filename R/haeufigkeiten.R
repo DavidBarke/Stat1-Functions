395da8fbae12e5f2bbdbf22ae33e1088c42ae951
David Barkemeyer <david.barkemeyer@gmx.de>
2021-02-24 22:23:23 +0100

    Added grouped_data

--- R/haeufigkeiten.R
+++ R/haeufigkeiten.R
@@ -9,8 +9,8 @@ sorted_data <- function(x) {
     dplyr::summarise(h_j = dplyr::n()) %>%
     dplyr::mutate(
       f_j = h_j / sum(h_j),
-      H = cumsum(h_j),
-      `F` = cumsum(f_j)
+      H_j = cumsum(h_j),
+      F_j = cumsum(f_j)
     )
 }
 
@@ -23,4 +23,74 @@ sorted_data <- function(x) {
 sorted_to_vector <- function(x, count) {
   stopifnot(length(x) == length(count))
   rep(x, times = count)
+}
+
+#' Summarise grouped data
+#'
+#' @param breaks A numeric vector with length *n + 1* where *n* is the number of
+#'   groups. Group *i* is the right-open interval [`breaks[i]`, `breaks[i + 1]`).
+#'   Group *n* is right-closed.
+#' @param x A numeric vector. See section `x_type`.
+#' @param x_type See section `x_type`.
+#' @param n Sample size (only used if `x_type = "f_j"`)
+#'
+#' @section x_type:
+#'
+#' Based on `x_type` the argument `x` is treated as follows:
+#'
+#' * `x` : Values to be grouped.
+#' * `h_j` : Absolute frequencies of each group. Then
+#'   `length(x) == length(breaks) - 1`.
+#' * `f_j` : Relative frequencies of each group. Then
+#'   `length(x) == length(breaks) - 1`. In this case the sample size `n` has
+#'   to be specified.
+#'
+#' @export
+grouped_data <- function(breaks, x, x_type = c("x", "h_j", "f_j"), n) {
+  x_type <- match.arg(x_type)
+
+  if (is.unsorted(breaks)) stop("'breaks' must be sorted!")
+
+  from <- breaks[1:(length(breaks) - 1)]
+  to <- breaks[2:length(breaks)]
+
+  tbl <- if (x_type == "x") {
+    stopifnot(all(x >= breaks[1] & x <= breaks[length(breaks)]))
+
+    groups <- findInterval(
+      x, breaks, rightmost.closed = TRUE
+    )
+
+    dplyr::tibble(
+      from = from,
+      to = to,
+      h_j = table(groups)
+    )
+  } else if (x_type == "h_j") {
+    stopifnot(length(x) == length(breaks) - 1)
+
+    dplyr::tibble(
+      from = from,
+      to = to,
+      h_j = x
+    )
+  } else {
+    stopifnot(length(x) == length(breaks) - 1)
+    if (sum(x) != 1) stop("Relative frequencies must add up to 1!")
+
+    dplyr::tibble(
+      from = from,
+      to = to,
+      h_j = x * n
+    )
+  }
+
+  tbl %>%
+    dplyr::mutate(
+      f_j = h_j / sum(h_j),
+      b_j = to - from,
+      m_j = (from + to) / 2,
+      H_j = cumsum(h_j),
+      F_j = cumsum(f_j)
+    )
 }
